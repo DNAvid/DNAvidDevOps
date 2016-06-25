94a3b838c1d03474a3fb138d863f54ef034b30cc
David Weiss <davidweisss@gmail.com>
2016-06-25 23:15:03 +0200

    Update aws_setup_scripts.sh

--- aws_setup_scripts.sh
+++ aws_setup_scripts.sh
@@ -6,3 +6,8 @@ sudo apt-get git
 ## https://docs.docker.com/machine/install-machine/
 curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
 chmod +x /usr/local/bin/docker-machine
+
+## Install AWS-CLI
+## ## Install pip
+sudo apt-get install python-pip
+
