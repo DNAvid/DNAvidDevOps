b6d4a8e48b0874611585fea8a1dfbf72666ee725
Ubuntu <ubuntu@ip-172-31-16-223.eu-central-1.compute.internal>
2016-06-29 10:18:13 +0000

    correct small errors

--- aws_setup_scripts.sh
+++ aws_setup_scripts.sh
@@ -5,7 +5,8 @@ sudo apt-get install git
 
 ## Install Docker-Machine
 ## https://docs.docker.com/machine/install-machine/
-curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > a
+sudo mv a /usr/local/bin/docker-machine
 chmod +x /usr/local/bin/docker-machine
 
 ## Install AWS-CLI
