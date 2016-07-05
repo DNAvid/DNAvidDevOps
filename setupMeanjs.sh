80fc0e7b538f225381200b0610235e55a9f8393f
David Weiss <davidweisss@gmail.com>
2016-07-05 23:10:52 +0200

    separate dev and prod containers and databases

--- setupMeanjs.sh
+++ setupMeanjs.sh
@@ -1,7 +1,13 @@
 ## Run Docker image for a MEANJS stack app
-## Create a new user
-sudo docker run -p 27017:27017 -d --name db mongo
-sudo docker run -p 80:3000 --link db:db_1 -ti meanjs/mean bash
-sudo docker run -p 80:3000 80:35729 --link db:db_1 -ti meanjs/mean bash
+## Dev
+sudo docker run -p 27017:27017 -d --name dbdev mongo
+sudo docker run -p 80:3000 --link dbdev:db_1 -ti --name meanjsdev meanjs/mean bash
+## Map livereload port??
+## sudo docker run -p 80:3000 80:35729 --link db:db_1 -ti meanjs/mean bash
 # When on meanjs/mean image
 grunt
+
+
+## Production
+sudo docker run -p 27017:27017 -d --name db mongo
+sudo docker run -p 80:8443 --link db:db_1 -ti --name meanjs meanjs/mean bash
