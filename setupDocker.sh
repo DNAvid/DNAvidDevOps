10f00ec62f8a12f99eea8dba7e18f670eefb7a51
David Weiss <davidweisss@gmail.com>
2016-07-05 21:28:16 +0200

    removed code specific to meanjs

--- setupDocker.sh
+++ setupDocker.sh
@@ -26,10 +26,3 @@ sudo service docker start
 ## Verify docker is installed correctly.
 sudo docker run hello-world
 
-## Run Docker image for a MEANJS stack app
-## Create a new user
-sudo docker run -p 27017:27017 -d --name db mongo
-sudo docker run -p 80:3000 --link db:db_1 -ti meanjs/mean bash
-sudo docker run -p 80:3000 80:35729 --link db:db_1 -ti meanjs/mean bash
-# When on meanjs/mean image
-grunt
