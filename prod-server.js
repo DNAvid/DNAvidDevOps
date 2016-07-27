bb0e36af577fdaf65937cf1fc610e938ca3ba283
David Weiss <davidweiss@dnavid.com>
2016-07-28 01:38:03 +0200

    Create prod-server.js

---
+++ prod-server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var fs = require('fs');
+var key = fs.readFileSync('./dnavid.key');
+var cert = fs.readFileSync('./dnavid.crt')
+var https = require('https');
+var http = require('http');
+var https_options = {
+    key: key,
+    cert: cert
+};
+var PORT = 443;
+var HOST = '0.0.0.0';
+app = express();
+app.get('/', function (req, res) {
+  res.sendFile('/home/davidweisss/dist/index.html')
+})
+app.use(express.static('/home/davidweisss/dist'));
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+// Redirect from http port 80 to https
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80);
