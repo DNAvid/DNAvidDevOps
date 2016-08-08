16807f200fe39e98706e7e3d04d66789b7096224
David Weiss <davidweiss@dnavid.com>
2016-08-08 19:22:24 +0200

    Update deploy-dnavid.sh

--- deploy-dnavid.sh
+++ deploy-dnavid.sh
@@ -1,7 +1,7 @@
 cd /home/davidweisss/auth0-react-sample/01-Login
 git pull
 npm install
-mv dist dist_hold
+sudo mv dist dist_hold
 sudo npm run build
 sudo cp dnavid.crt dnavid.key server.js dist/
 cd dist
