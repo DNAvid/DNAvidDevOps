2c22cc1bb2789209383a82c636263a979ff9e416
David Weiss <davidweiss@dnavid.com>
2017-01-10 00:29:35 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,9 @@
+# get the mongo docker
+sudo docker pull mongo
+
+## Start a docker container with mongo
+sudo docker run -p 27017:27017 --name dnavid -d mongo
+
 ## connect to MongoDB Shell
 sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017
 _TCP_PORT/test"'
