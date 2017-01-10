831843ed81b8ecad19bcd3ba4ad61db1163d48d0
David Weiss <davidweiss@dnavid.com>
2017-01-10 01:36:01 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -4,13 +4,17 @@ sudo docker pull mongo
 ## Start a docker container with mongo
 sudo docker run -p 27017:27017 --name dnavid -d mongo
 
-## connect to MongoDB Shell
+## connect to MongoDB Shell w/o uname and pwd
 sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017
 _TCP_PORT/test"'
 
+## connect to MongoDB Shell w/ uname and pwd
+sudo docker run -it --link dnavid:mongo --rm mongo sh -u dnavid -p some-initial-password --authenticationDatabase admin-c 'exec mongo "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017
+_TCP_PORT/test"'
+
 ## Load json into db
-mongoimport --db test --collection restaurants --drop --file ~/downloads/primer-dataset.json
+mongoimport --db test --collection users --drop --file ~/users.json
 
 
 ## Configure secure login (initial password is secret)
-> db.createUser({ user: 'dnavid', pwd: 'some-initial-password', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
+## > db.createUser({ user: 'dnavid', pwd: 'some-initial-password', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
