c0c4bb65b917a5ba9cc4987e612f5c874c503311
David Weiss <davidweiss@dnavid.com>
2017-01-10 19:12:42 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,8 @@
-# Secure version
+# Secure version of mongo docker container
 ## https://hub.docker.com/r/khezen/mongo/
-## sudo docker run -d -p 27017:27017 --name dnavid khezen/mongo:latest
 sudo docker pull khezen/mongo
-sudo docker run -d -p 27017:27017 -e ADMIN_USER=admin -e ADMIN_PWD=secret-password -e AUTH=y --name dnavid khezen/mongo:latest
-sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo -u admin -p secret-password  "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/admin"'
+sudo docker run -d -p 27017:27017 -e ADMIN_USER=root -e ADMIN_PWD=secret-password -e AUTH=y --name dnavid khezen/mongo:latest
+sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo -u root -p secret-password  "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/admin"'
 
 ## Create user inside
 > use dnavid
@@ -14,6 +13,6 @@ sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo -u admin -p
         roles: [ { role: "readWrite", db: "dnavid" } ]
 }
 )
-
+## not possible to log in with this user ;-(
 ## Cleanup
 ## sudo docker stop dnavid; sudo docker rm dnavid
