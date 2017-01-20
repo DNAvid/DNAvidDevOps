eef7cd5155597b5360b78e89b09f00e4eb3a7ec2
David Weiss <davidweiss@dnavid.com>
2017-01-20 22:13:25 +0100

    create user and public dbs and public and api users

--- mongodb.sh
+++ mongodb.sh
@@ -4,15 +4,30 @@ sudo docker pull khezen/mongo
 sudo docker run -d -p 27017:27017 -e ADMIN_USER=root -e ADMIN_PWD=secret-password -e AUTH=y --name dnavid khezen/mongo:latest
 sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo -u root -p secret-password  "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/admin"'
 
-## Create user inside
-> use dnavid
+## Create users database
+> use users
+## Create api user, behind authentication and authorization to read and write from user database
 > db.createUser(
 {
         user: "api",
         pwd: "api-secret-password",
-        roles: [ { role: "readWrite", db: "dnavid" } ]
+        roles: [ 
+        { role: "readWrite", db: "users" } ,
+        { role: "readWrite", db: "public" }         
+        ]
 }
 )
-## not possible to log in with this user ;-(
+
+## Create public user, reading information made public by users on public database
+> use public
+> db.createUser(
+{
+        user: "public",
+        pwd: "public-secret-password",
+        roles: [ { role: "read", db: "public" } ]
+}
+)
+
+
 ## Cleanup
 ## sudo docker stop dnavid; sudo docker rm dnavid
