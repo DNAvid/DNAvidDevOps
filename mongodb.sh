988203d855c748586c6533b15d816b0ac6b86cc6
David Weiss <davidweiss@dnavid.com>
2017-01-20 22:50:41 +0100

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,9 @@ sudo docker pull khezen/mongo
 sudo docker run -d -p 27017:27017 -e ADMIN_USER=root -e ADMIN_PWD=secret-password -e AUTH=y --name dnavid khezen/mongo:latest
 sudo docker run -it --link dnavid:mongo --rm mongo sh -c 'exec mongo -u root -p secret-password  "$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/admin"'
 
+## Cleanup
+## sudo docker stop dnavid; sudo docker rm dnavid
+
 ## Create users database
 > use users
 ## Create api user, behind authentication and authorization to read and write from user database
